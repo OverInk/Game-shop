645d21851b6257714a4ffcc36a08917a85238f34
OverInk <dostoevskiy201@mail.ru>
2023-09-24 21:38:48 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const name = 'Vasya'
+
+	function test() {
+		return 'Teastovaya info'
+	}
+
+	return (
+		<div className="App">
+			<div> Hello world!</div>
+			{/* {alert(123)} */}
+			{/* {1 + 1} */}
+			{name}
+			{test()}
+		</div>
+	);
 }
 
 export default App;
