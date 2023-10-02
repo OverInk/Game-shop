1080e74bd67c9c43ef5bb24624db6243ee1df4e4
OverInk <dostoevskiy201@mail.ru>
2023-10-02 16:36:35 +0300

    New

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import Header from './components/Header';
 import Categor from './components/Categor';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
+import Skeleton from './components/PizzaBlock/Skeleton';
 
 function App() {
   const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch('https://6516b50209e3260018ca2dff.mockapi.io/items')
@@ -16,6 +18,7 @@ function App() {
       })
       .then((arr) => {
         setItems(arr);
+		  setIsLoading(false);
       });
   }, []);
 
@@ -30,9 +33,15 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            <PizzaBlock title="Мексиканская" price={500} />
+            <Skeleton title="Мексиканская" price={500} />
             <PizzaBlock test="222" title="Test" price="350" />
-            {items.map((object) => (
+				{isLoading ? [...new Array(6)].map((_, index) => <Skeleton key={index} />) : items.map((object) => <PizzaBlock key={object.id}
+                title={object.title}
+                price={object.price}
+                imgUrl={object.img}
+                sizes={object.sizes}
+                types={object.types} /> )}
+            {/* {items.map((object) => (
               <PizzaBlock
                 key={object.id}
                 title={object.title}
@@ -41,7 +50,7 @@ function App() {
                 sizes={object.sizes}
                 types={object.types}
               />
-            ))}
+            ))} */}
           </div>
         </div>
       </div>
