28ef31a90b3083bf7f20f48598eb13e62e1bbfcc
OverInk <dostoevskiy201@mail.ru>
2023-10-23 12:39:37 +0300

    New

--- src/redux/slices/gamesSlice.js
+++ src/redux/slices/gamesSlice.js
@@ -1,14 +1,19 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-export const fetchGamesAsync = createAsyncThunk('games/fetchGamesStatus', async (params) => {
+export const fetchGamesAsync = createAsyncThunk('games/fetchGamesStatus', async (params, thunkAPI) => {
   const { sortBy, categorId, currentPage } = params;
   const { data } = await axios.get(
     `https://6516b50209e3260018ca2dff.mockapi.io/items?page=${currentPage}&limit=3${
       categorId > 0 ? `category=${categorId}` : ''
     }&sortBy=${sortBy}&order=desc`,
   );
-  return data;
+
+  if (data.length === 0) {
+	return thunkAPI.rejectWithValue('Игры пустые');
+  }
+
+  return thunkAPI.fulfillWithValue(data)  ;
 });
 
 export const initialState = {
@@ -30,10 +35,12 @@ const gamesSlice = createSlice({
       state.items = [];
     });
     builder.addCase(fetchGamesAsync.fulfilled, (state, action) => {
+		console.log(action, 'fullfilled')
       state.items = action.payload;
       state.status = 'success';
     });
     builder.addCase(fetchGamesAsync.rejected, (state, action) => {
+		console.log(action, 'regected')
       state.status = 'error';
       state.items = [];
     });
