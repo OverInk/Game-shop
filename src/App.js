5169f3c8b1581dd876a884d6714f427013f9a2e7
OverInk <dostoevskiy201@mail.ru>
2023-10-25 23:31:57 +0300

    New

--- src/App.js
+++ src/App.js
@@ -7,22 +7,32 @@ import NotFound from './pages/NotFound';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NewCard from './pages/NewCard';
 import NewFullGame from './pages/NewFullGame';
+import MainLayout from './layouts/MainLayout';
 
 function App() {
   return (
-    <div className="wrapper">
-      <Header />
-      <div className="content">
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/card" element={<NewCard />} />
-				<Route path="/game/:id" element={<NewFullGame />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route path="" element={<Home />} />
+        <Route path="card" element={<NewCard />} />
+        <Route path="game/:id" element={<NewFullGame />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+
+    //  <div className="wrapper">
+    //    <Header />
+    //    <div className="content">
+    //      <div className="container">
+    //        <Routes>
+    //          <Route path="/" element={<Home />} />
+    //          <Route path="/card" element={<NewCard />} />
+    // 			<Route path="/game/:id" element={<NewFullGame />} />
+    //          <Route path="*" element={<NotFound />} />
+    //        </Routes>
+    //      </div>
+    //    </div>
+    //  </div>
   );
 }
 export default App;
--- src/pages/NewFullGame.jsx
+++ src/pages/NewFullGame.jsx
@@ -1,22 +1,37 @@
-import axios from "axios";
-import React from "react";
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
 
 const NewFullGame = () => {
-	const {id} = useParams();
+  const { id } = useParams();
+  const [game, setGame] = useState();
+  const navigate = useNavigate();
 
-	// useEffect(() => {
-	// 	axios.get('https://6516b50209e3260018ca2dff.mockapi.io/items' + id)
-	// }, [])
+  useEffect(() => {
+    async function fetchGames() {
+      try {
+        const { data } = await axios.get('https://6516b50209e3260018ca2dff.mockapi.io/items/' + id);
+        setGame(data);
+      } catch (error) {
+        alert('Ошибка при получении игр!');
+        navigate('/');
+      }
+    }
+    fetchGames();
+  }, []);
 
-	return ( 
-		<div>
-			<h2>Game 1</h2>
-			<h3> Тут мой id через params{id}</h3>
-			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum ullam fuga, quod porro placeat iste delectus sunt doloribus quis? Quisquam delectus est culpa. Maiores, adipisci dignissimos. Nobis corporis nemo doloribus?</p>
-		</div>
-	 );
-}
- 
+  if (!game) {
+    return 'Загрузка..';
+  }
+
+  return (
+    <div>
+      <h2>{game.title}</h2>
+      <h3> Тут мой id через params{id}</h3>
+      <h4>{game.price} </h4>
+    </div>
+  );
+};
+
 export default NewFullGame;
