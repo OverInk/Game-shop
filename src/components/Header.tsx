57f7c0e5594b8faea52ab83bd39fec5c21bad81a
OverInk <dostoevskiy201@mail.ru>
2023-11-09 17:01:27 +0300

    New

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,9 +9,18 @@ function Header() {
   const { items, totalPrice } = useSelector(selectCart);
 
   const location = useLocation();
+  const isMounted = React.useRef(false)
 
   const totalCount = items.reduce((sum:number, item:any) => sum + item.count, 0);
 
+
+	React.useEffect(() => {
+		if (isMounted.current) {
+			const json = JSON.stringify(items)
+			localStorage.setItem('cart', json)
+		}
+		isMounted.current = true;
+	}, [items])
   return (
     <div className="header">
       <div className="container">
