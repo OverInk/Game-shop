ded920c8f57345f956eca5fa6cf3c96a2372b94c
OverInk <dostoevskiy201@mail.ru>
2023-11-22 23:50:06 +0300

    Update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -28,8 +28,8 @@ function Header() {
           <div className="header__logo">
             <img width="38" src={logoSvg} alt="Pizza logo" />
             <div>
-              <h1>React Pizza</h1>
-              <p>самая вкусная пицца во вселенной</p>
+              <h1>React Games</h1>
+              <p>самая класнные игры в мире</p>
             </div>
           </div>
         </Link>
